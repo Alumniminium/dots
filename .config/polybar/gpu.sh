bd8611f3879d8000556d68cf01f8080cba3ce646
trbl <trbl@her.st>
2020-08-13 22:22:19 +0200

    better gpu load monitoring in polybar

---
+++ .config/polybar/gpu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+samples=10
+
+for ((;;))
+do
+    total=0
+    for i in $(seq 1 $samples)
+    do
+        sample=$(cat /sys/class/drm/card0/device/gpu_busy_percent)
+        total=$((total + sample))
+        sleep 0.1
+    done
+    echo "GPU: $((total / samples))%"
+done
