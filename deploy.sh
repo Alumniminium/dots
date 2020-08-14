da9ca8c4e5b344cf60105b9241f01f2dd36bc3f3
trbl <trbl@her.st>
2020-08-14 12:01:25 +0200

    deploy script seems to work

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-yay -S rmtrash light acpi_call exa tp_smapi pulseeffects-git picom-tyrone-git 
+
+#if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+#    echo "Give me root!"
+#    exit
+#fi
+
+yay --save --nocleanmenu --nodiffmenu --noconfirm
+
+yes | yay -S rmtrash light acpi_call exa tp_smapi pulseeffects-git picom-tyrone-git 
 # optional apps
-yay -S keynav ncdu dfc pkgfile
+yes | yay -S keynav ncdu dfc pkgfile mtr
 # optional wine deps
-yay -S lib32-giflib lib32-mpg123 lib32-v4l-utils lib32-libxslt lib32-gtk3
+yes | yay -S lib32-giflib lib32-mpg123 lib32-v4l-utils lib32-libxslt lib32-gtk3
 # c# dev
-yay -S dotnet-host dotnet-runtime dotnet-sdk
+yes | yay -S dotnet-host dotnet-runtime dotnet-sdk
 
 # install font
 cp Assets/ProFontWindows.ttf /usr/share/fonts/TTF
 # install background
 cp Assets/.bg ~/.bg
+
+
+cp -r .config ~
+cp -r etc /
+cp -r .local ~
