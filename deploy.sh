8a98037fa5c9d3dcf22ec86716f7598ddc82b681
trbl <trbl@her.st>
2020-08-17 16:48:11 +0200

    rewrote imgup in bash

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 yay --save --nocleanmenu --nodiffmenu --noconfirm
 
-yes | yay -S rmtrash light acpi_call exa tp_smapi pulseeffects-git picom-tyrone-git 
+yes | yay -Syu
+yes | yay -S rmtrash light acpi_call exa tp_smapi pulseeffects-git picom-tyrone-git xclip
 # optional apps
 yes | yay -S keynav ncdu dfc pkgfile mtr
 # optional wine deps
@@ -10,8 +10,6 @@ yes | yay -S lib32-giflib lib32-mpg123 lib32-v4l-utils lib32-libxslt lib32-gtk3
 # c# dev
 yes | yay -S dotnet-host dotnet-runtime dotnet-sdk
 
-# install font
-cp Assets/ProFontWindows.ttf /usr/share/fonts/TTF
 # install background
 cp Assets/.bg ~/.bg
 
@@ -20,3 +18,7 @@ cp -r .config ~
 cp -r .local ~
 echo 'copying /etc gimme root!'
 sudo cp -r etc /
+
+# install font
+echo 'copying //usr/share/fonts/TTF gimme root!'
+sudo cp Assets/ProFontWindows.ttf /usr/share/fonts/TTF
