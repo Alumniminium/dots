3eb3095411b67000eaea54402e8b199614acbdc6
trbl <trbl@her.st>
2020-08-19 11:43:32 +0200

    some missing scripts, update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
+#bypass installing apps if any arg is passed (fast path)
 if [ -z $1 ]; then
     yay --save --nocleanmenu --nodiffmenu --noconfirm
     yes | yay -Syu
-    yes | yay -S rmtrash light acpi_call exa tp_smapi pulseeffects-git picom-tyrone-git xclip sox
-    # optional apps
-    yes | yay -S keynav ncdu dfc pkgfile mtr
-    # optional wine deps
-    yes | yay -S lib32-giflib lib32-mpg123 lib32-v4l-utils lib32-libxslt lib32-gtk3
-    # c# dev
-    yes | yay -S dotnet-host dotnet-runtime dotnet-sdk
-
-    # install background
-    cp Assets/.bg ~/.bg
-    cp Assets/ding.wav ~/.config/
+    yes | yay -S linux-amd rmtrash light acpi_call exa tp_smapi pulseeffects-git picom-tyrone-git xclip sox rmtrash ncdu dfc pkgfile mtr dotnet-host dotnet-runtime dotnet-sdk
+    # optional stuff (wine deps, ..)
+    yes | yay -S lib32-giflib lib32-mpg123 lib32-v4l-utils lib32-libxslt lib32-gtk3 keynav 
 fi
 
+# install background
+cp Assets/.bg ~/.bg
+cp Assets/ding.wav ~/.config/
 
 cp -r .config ~
 cp -r .local ~
