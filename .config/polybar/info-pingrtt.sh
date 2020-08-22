48bc195d811224777dce02ed1d4d80a71feff8dd
trbl <trbl@her.st>
2020-08-22 09:08:00 +0200

    reduced pings

--- .config/polybar/info-pingrtt.sh
+++ .config/polybar/info-pingrtt.sh
@@ -2,7 +2,7 @@
 
 HOST=89.163.230.146
 
-if ! ping=$(ping -n -c 1 -W 1 $HOST); then
+if ! ping=$(ping -n -c 1 -W 5 $HOST); then
     echo "# ping failed"
 else
     rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
