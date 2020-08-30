1a6c0e7ca1bd5c595910458ca22acb5fe3cbd68a
trbl <trbl@her.st>
2020-08-30 14:49:21 +0200

    my mod of lambda-mod oh-my-zsh theme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dots
-configs and scripts for archlinux
+for my [Lenovo IdeaPad 5 15are05](https://wiki.archlinux.org/index.php/Lenovo_IdeaPad_5_15are05) running arch/bspwm.
+
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
+cp -s $PWD/.xinitrc "/home/$(whoami)/"
+cp -s $PWD/.zprofile "/home/$(whoami)/"
+cp -s $PWD/.zshrc "/home/$(whoami)/"
+cp -s $PWD/.oh-my-zsh/themes/* "/home/$(whoami)/.oh-my-zsh/themes/"
 # install background
-cp -s $PWD/Assets/.bg "/home/$(whoami)/.bg"
+cp -s $PWD/Assets/.bg "/home/$(whoami)/"
 cp -s $PWD/Assets/ding.wav "/home/$(whoami)/.config/"
 
 # install ocnfigs
@@ -26,12 +29,13 @@ if [ -z $1 ]; then
     yay --save --nocleanmenu --nodiffmenu --noconfirm
     yes | yay -Syu
     yes | yay -S linux-amd acpi_call-dkms rmtrash light exa tp_smapi pulseeffects-git \
-                 picom-tryone-git xclip sox rmtrash ncdu dfc \
+                 picom-tryone-git xclip sox rmtrash ncdu dfc zsh \
                  pkgfile mtr dotnet-host dotnet-runtime dotnet-sdk \
                  visual-studio-code-bin chromium-vaapi network-manager-applet \
                  discord thunderbird nemo
-
     # optional stuff (wine deps, ..)
-    yes | yay -S lib32-giflib lib32-mpg123 lib32-v4l-utils lib32-libxslt lib32-gtk3 keynav 
+    yes | yay -S lib32-giflib lib32-mpg123 lib32-v4l-utils lib32-libxslt lib32-gtk3 keynav
+
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
